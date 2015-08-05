022b309d3271ea0147924138160ae850e38085ba
cindyeleana <eli92_sp@hotmail.com>
2015-08-04 21:29:27 -0500

    Vista de menú

--- ProyectoDAW2P/ProyectoDAW2P/urls.py
+++ ProyectoDAW2P/ProyectoDAW2P/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from main.views import IndexView
+from main import views
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', IndexView.as_view()),
+     url(r'^$', views.IndexView.as_view()),
+    url(r'^menu/$', views.MenuView.as_view()),
 )
--- ProyectoDAW2P/main/views.py
+++ ProyectoDAW2P/main/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 class IndexView(TemplateView):
 	template_name = "index.html"
+
+	def post(self, request):
+		return HttpResponseRedirect('/menu/')
+
+
+class MenuView(TemplateView):
+	template_name = "menu.html"
